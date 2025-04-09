3d9724ef2366b5b7f59b3b3a1db4b758d7bdb231
Killowa <serikakhmetovadil@mail.ru>
2025-04-09 19:31:44 +0500

    changed service.java

--- app/src/main/java/com/example/lab82/MyService.java
+++ app/src/main/java/com/example/lab82/MyService.java
@@ -1,4 +1,68 @@
 package com.example.lab82;
 
-public class MyService {
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+public class MyService extends Service {
+    private MediaPlayer soundPlayer;
+    private final String CHANNEL_ID = "channelId";
+
+    @Override
+    public void onCreate() {
+        Toast.makeText(this, "Service Created", Toast.LENGTH_SHORT).show();
+        soundPlayer = MediaPlayer.create(this, R.raw.song);
+        soundPlayer.setLooping(true);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        createNotificationChannel();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_music)
+                .setContentTitle("Music Playing")
+                .setContentText("Слушаем музыку 🎶")
+                .build();
+
+        startForeground(1, notification);
+
+        soundPlayer.start();
+
+        return START_STICKY;
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Music Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        soundPlayer.stop();
+        Toast.makeText(this, "Service Stopped", Toast.LENGTH_SHORT).show();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
