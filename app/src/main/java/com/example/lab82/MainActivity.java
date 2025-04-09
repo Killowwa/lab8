c76fe0cce99c5e8e3a012ce74d38d52ae8586d85
Killowa <serikakhmetovadil@mail.ru>
2025-04-09 23:22:54 +0500

    add activity & service to manifest

--- app/src/main/java/com/example/lab82/MainActivity.java
+++ app/src/main/java/com/example/lab82/MainActivity.java
@@ -4,16 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
     private Intent foregroundServiceIntent;
-    private Intent backgroundServiceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         foregroundServiceIntent = new Intent(this, MyService.class);
-        backgroundServiceIntent = new Intent(this, RandomCharacterService.class);
     }
 
     public void startForegroundService(View view) {
@@ -32,15 +27,8 @@ public class MainActivity extends AppCompatActivity {
         stopService(foregroundServiceIntent);
     }
 
-    public void startBackgroundService(View view) {
-        startService(backgroundServiceIntent);
-    }
-
-    public void stopBackgroundService(View view) {
-        stopService(backgroundServiceIntent);
-    }
-
     public void nextActivity(View view) {
-        startActivity(new Intent(this, MainActivity2.class));
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
