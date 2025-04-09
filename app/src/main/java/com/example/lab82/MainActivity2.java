0acb38857b185bbb835d60d8bdd1ba3bab282f08
Killowa <serikakhmetovadil@mail.ru>
2025-04-09 23:19:46 +0500

    update main2.java & main2.xml

--- app/src/main/java/com/example/lab82/MainActivity2.java
+++ app/src/main/java/com/example/lab82/MainActivity2.java
@@ -1,14 +1,28 @@
 package com.example.lab82;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private Intent backgroundServiceIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        backgroundServiceIntent = new Intent(this, RandomCharacterService.class);
+
+        Button startButton = findViewById(R.id.button_start_background);
+        Button stopButton = findViewById(R.id.button_stop_background);
+
+        startButton.setOnClickListener(v -> startService(backgroundServiceIntent));
+        stopButton.setOnClickListener(v -> stopService(backgroundServiceIntent));
     }
 }
 
